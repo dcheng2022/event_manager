57ed5717a6c3ac88e86e227236eeb9a9ed9e20b4
Darin <darinchengg@gmail.com>
2022-07-04 14:17:54 -0500

    Add clean_phone_number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,15 @@ def legislators_by_zipcode(zip)
   end
 end
 
+def clean_phone_number(number)
+  number.gsub!(/\D/, '')
+  if number.length == 11 && number.start_with?('1')
+    number[1..10]
+  elsif number.length == 10
+    number
+  end
+end
+
 def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
@@ -47,8 +56,10 @@ contents.each do |row|
   name = row[:first_name]
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
+  phone = clean_phone_number(row[:homephone])
 
   form_letter = erb_template.result(binding)
 
   save_thank_you_letter(id, form_letter)
 end
+
